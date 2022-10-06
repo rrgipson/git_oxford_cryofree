148174a29a7d580f1c56a3581cec966b0b18e142
rrgipson <91292902+rrgipson@users.noreply.github.com>
2022-10-05 17:00:48 -0700

    Fixed field setpt in vtvh log
    Also start of 1st run in VTVH log!

--- main_new.py
+++ main_new.py
@@ -845,7 +845,7 @@ class Application:
         #setup logging
         self.vtvh_logger.set_log_file('UserLogs/vtvh_log.csv')
         if self._field_connect:
-            self.vtvh_logger.assign_field_log_fxns(get_mag_temp=self.get_magnet_temp, get_mag_field=self.get_current_magnet_field)
+            self.vtvh_logger.assign_field_log_fxns(get_mag_temp=self.get_magnet_temp, get_mag_field=self.get_current_magnet_field, get_field_set=self.get_field)
         if self._temp_connect:
              self.vtvh_logger.assign_temp_log_fxns(get_vti_temp=self.get_vti_temp, get_sample_temp=self.get_sample_temp, get_pt2_temp=self.get_pt2_temp, get_nv_pressure=self.get_nv_pressure, get_nv_percent=self.get_nv_percent, get_temp_set=self.get_temperature)
             
