802fbb9404c14761604117b69c95c3329441c220
rrgipson <91292902+rrgipson@users.noreply.github.com>
2023-01-16 12:03:14 -0800

    New ahk for waiting for scan to end

--- .ipynb_checkpoints/spectrometer-checkpoint.py
+++ .ipynb_checkpoints/spectrometer-checkpoint.py
@@ -35,6 +35,5 @@ def initiate_scan_onelamp():
     
 def turn_off_xe_lamp():
     print('Turning Off J-1700 Xe-Arc Lamp')
-    #just added an additional enter press in order to close the popup asking for both lamps to be lit
     autohotkey_event = subprocess.run((autohotkey_exe, 'lib/J1700_XeLamp_off.ahk'), capture_output=True) ## Edited by Rob
     print(autohotkey_event.stdout.decode('utf-8').replace('\n',''))
